783e3914b70128a6441a293b65042288a2a7698b
Lakith Rathnayake <lakithpathmila@gmail.com>
2024-03-14 01:35:26 +0530

    Implement GET web services and DELETE web service

--- src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
+++ src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
@@ -13,9 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/lecturers")
@@ -67,7 +71,10 @@ public class LecturerHttpController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping(value = "/{lecturer-id}", consumes = "multipart/form-data")
-    public void updateLecturerDetailsViaMultipart(@PathVariable("lecturer-id") Integer lecturerId){}
+    public void updateLecturerDetailsViaMultipart(@PathVariable("lecturer-id") Integer lecturerId){
+        Lecturer lecturer = em.find(Lecturer.class, lecturerId);
+        if(lecturer == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping(value = "/{lecturer-id}", consumes = "application/json")
@@ -75,18 +82,66 @@ public class LecturerHttpController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping({"/{lecturer-id}"})
-    public void deleteLecturer(@PathVariable("lecturer-id") Integer lecturerId){}
+    public void deleteLecturer(@PathVariable("lecturer-id") Integer lecturerId){
+        Lecturer lecturer = em.find(Lecturer.class, lecturerId);
+        if(lecturer == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        em.getTransaction().begin();
+        try {
+            em.remove(lecturer);
+            if(lecturer.getPicture() != null) {
+                bucket.get(lecturer.getPicture().getPicturePath()).delete();
+            }
+
+            em.getTransaction().commit();
+        } catch (Throwable e) {
+            em.getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
+    }
 
     @GetMapping(produces = "application/json")
-    public void getAllLecturers(){}
+    public List<LecturerTO> getAllLecturers(){
+        TypedQuery<Lecturer> query = em.createQuery("SELECT l FROM Lecturer l", Lecturer.class);
+        return getLecturerTOList(query);
+    }
 
     @GetMapping(value = "/{lecturer-id}", produces = "application/json")
-    public void getLecturerDetails(@PathVariable("lecturer-id") Integer lectureId){}
+    public LecturerTO getLecturerDetails(@PathVariable("lecturer-id") Integer lectureId){
+        Lecturer lecturer = em.find(Lecturer.class, lectureId);
+        if(lecturer == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        LecturerTO lecturerTO = mapper.map(lecturer, LecturerTO.class);
+        if(lecturer.getLinkedIn() != null){
+            lecturerTO.setLinkedin(lecturer.getLinkedIn().getUrl());
+        }
+        if(lecturer.getPicture() != null) {
+            lecturerTO.setPicture(bucket.get(lecturer.getPicture().getPicturePath()).signUrl(1, TimeUnit.DAYS).toString());
+        }
+        return lecturerTO;
+    }
 
     @GetMapping(params = "type=full-time", produces = "application/json")
-    public void getFullTimeLecturers(){}
+    public List<LecturerTO> getFullTimeLecturers(){
+        TypedQuery<Lecturer> query = em.createQuery("SELECT l FROM Lecturer l WHERE l.type = lk.ijse.dep11.edupanel.util.LecturerType.FULL_TIME", Lecturer.class);
+        return getLecturerTOList(query);
+    }
 
     @GetMapping(params = "type=visiting", produces = "application/json")
-    public void getVisitingLecturers(){}
+    public List<LecturerTO> getVisitingLecturers(){
+        TypedQuery<Lecturer> query = em.createQuery("SELECT l FROM Lecturer l WHERE l.type = lk.ijse.dep11.edupanel.util.LecturerType.VISITING", Lecturer.class);
+        return getLecturerTOList(query);
+    }
+
+    private List<LecturerTO> getLecturerTOList(TypedQuery<Lecturer> query) {
+        return query.getResultStream().map(lecturerEntity -> {
+            LecturerTO lecturerTO = mapper.map(lecturerEntity, LecturerTO.class);
+            if(lecturerEntity.getLinkedIn() != null){
+                lecturerTO.setLinkedin(lecturerEntity.getLinkedIn().getUrl());
+            }
+            if(lecturerEntity.getPicture() != null) {
+                lecturerTO.setPicture(bucket.get(lecturerEntity.getPicture().getPicturePath()).signUrl(1, TimeUnit.DAYS).toString());
+            }
+            return lecturerTO;
+        }).collect(Collectors.toList());
+    }
 
 }
