89cd5683a0155c93e861dd6f098b6f99da4d9020
Lakith Rathnayake <lakithpathmila@gmail.com>
2024-03-25 19:17:28 +0530

    Add aspect to handle store

--- src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
+++ src/main/java/lk/ijse/dep11/edupanel/api/LecturerHttpController.java
@@ -35,13 +35,12 @@ public class LecturerHttpController {
     @Autowired
     private Bucket bucket;
 
+    private LecturerService lecturerService;
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = "multipart/form-data", produces = "application/json")
     public LecturerTO createNewLecturer(@ModelAttribute @Validated(LecturerReqTO.Create.class) LecturerReqTO lecturerReqTO){
 
-        AppStore.setBucket(bucket);
-        AppStore.setEntityManager(em);
-        LecturerService lecturerService = ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.LECTURER);
         return lecturerService.saveLecturer(lecturerReqTO);
     }
 
@@ -49,61 +48,40 @@ public class LecturerHttpController {
     @PatchMapping(value = "/{lecturer-id}", consumes = "multipart/form-data")
     public void updateLecturerDetailsViaMultipart(@PathVariable("lecturer-id") Integer lecturerId,
                                                   @ModelAttribute @Validated(LecturerReqTO.Update.class) LecturerReqTO lecturerReqTO){
-        AppStore.setBucket(bucket);
-        AppStore.setEntityManager(em);
         lecturerReqTO.setId(lecturerId);
-        LecturerService lecturerService = ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.LECTURER);
         lecturerService.updateLecturerDetails(lecturerReqTO);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping(value = "/{lecturer-id}", consumes = "application/json")
     public void updateLecturerDetailsViaJson(@PathVariable("lecturer-id") Integer lecturerId, @RequestBody @Validated LecturerTO lecturerTO){
-        AppStore.setBucket(bucket);
-        AppStore.setEntityManager(em);
         lecturerTO.setId(lecturerId);
-        LecturerService lecturerService = ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.LECTURER);
         lecturerService.updateLecturerDetails(lecturerTO);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping({"/{lecturer-id}"})
     public void deleteLecturer(@PathVariable("lecturer-id") Integer lecturerId){
-        AppStore.setBucket(bucket);
-        AppStore.setEntityManager(em);
-        LecturerService lecturerService = ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.LECTURER);
         lecturerService.deleteLecturer(lecturerId);
     }
 
     @GetMapping(produces = "application/json")
     public List<LecturerTO> getAllLecturers(){
-        AppStore.setBucket(bucket);
-        AppStore.setEntityManager(em);
-        LecturerService lecturerService = ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.LECTURER);
         return lecturerService.getLecturers(null);
     }
 
     @GetMapping(value = "/{lecturer-id}", produces = "application/json")
     public LecturerTO getLecturerDetails(@PathVariable("lecturer-id") Integer lectureId){
-        AppStore.setBucket(bucket);
-        AppStore.setEntityManager(em);
-        LecturerService lecturerService = ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.LECTURER);
         return lecturerService.getLecturerDetails(lectureId);
     }
 
     @GetMapping(params = "type=full-time", produces = "application/json")
     public List<LecturerTO> getFullTimeLecturers(){
-        AppStore.setBucket(bucket);
-        AppStore.setEntityManager(em);
-        LecturerService lecturerService = ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.LECTURER);
         return lecturerService.getLecturers(LecturerType.FULL_TIME);
     }
 
     @GetMapping(params = "type=visiting", produces = "application/json")
     public List<LecturerTO> getVisitingLecturers(){
-        AppStore.setBucket(bucket);
-        AppStore.setEntityManager(em);
-        LecturerService lecturerService = ServiceFactory.getInstance().getService(ServiceFactory.ServiceType.LECTURER);
         return lecturerService.getLecturers(LecturerType.VISITING);
     }
 }
